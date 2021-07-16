2d5c1a24840c56643ade64f54d7b93a9491d4b08
Chris Peter Francis <Chrispyontheoutside@users.noreply.github.com>
2021-07-15 21:01:08 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,12 +8,8 @@ app = Flask(__name__)
 @app.route("/")
 def fortune_cookie():
     ## Get the request from the website
-    response = requests.get("https://api.adviceslip.com/advice")
     ## Get the json
-    advice = response.json()
     ## Get the advice from inside the slip
-    advice = advice["slip"]["advice"]
     #Give our HTML file the slip
-    return render_template('index.html',advice = advice)
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port = 8080)
